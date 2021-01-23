d061b43241112a47bc6bd415e4282f8f7d29d349
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-23 14:11:22 +0100

    some cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <Footer />
     </div>
   );
