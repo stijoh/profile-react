92ad7aa05aaaefabf42ca25be14eae67ce26fcef
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-26 20:26:29 +0100

    refactored the login form and made a component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Formik } from "formik";
+import { TextField, Button } from "@material-ui/core";
+
+
+const LoginForm = () => {
+
+    return (
+
+      <div>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={async (values, actions) => {
+            const requestOptions = {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(
+                {
+                  user: {
+                    email: values.email,
+                    password: values.password
+                  }
+                }),
+            };
+            const response = await fetch(
+              "http://192.168.0.100:3500/api/v1/sign_in",
+              requestOptions
+            );
+            const data = await response.json();
+            console.log(data.data.user);
+            localStorage.setItem("token", data.data.user.authentication_token)
+
+          }}
+        >
+          {props => (
+            <form onSubmit={props.handleSubmit}>
+              <div>
+                <TextField
+                  type="text"
+                  onChange={props.handleChange}
+                  onBlur={props.handleBlur}
+                  value={props.values.name}
+                  name="email"
+                />
+                {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+              </div>
+              <div>
+                <TextField
+                  type="password"
+                  onChange={props.handleChange}
+                  onBlur={props.handleBlur}
+                  value={props.values.name}
+                  name="password"
+                />
+                {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+              </div>
+              <div>
+                <Button type="submit">Submit</Button>
+              </div>
+            </form>
+          )}
+        </Formik>
+      </div>
+    )
+  }
+
+export default LoginForm;
--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,69 +1,17 @@
 import React from "react";
-import { Formik } from "formik";
-import { TextField, Button } from "@material-ui/core";
-
+import LoginForm from "../../components/LoginForm";
 
 const Admin = () => {
-    const loginData = () => {
-        return "balle";
-    };
-    console.log(JSON.stringify(loginData));
-
-    const login = async () => {
-        const requestOptions = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(loginData),
-        };
-        const response = await fetch(
-            "http://127.0.0.1:3500/api/v1/sign_in",
-            requestOptions
+    if (!localStorage.getItem("token")) {
+        return (
+            <div>
+                <LoginForm />
+            </div>
         );
-        const data = await response.json();
-        console.log(data);
-    };
-    return (
-      <div>
-        <div>Hello from admin ballefaen!</div>
-          <div>
-          <Formik
-            initialValues={{ Email: '', Password: '' }}
-            onSubmit={(values, actions) => {
-              console.log(values)
-
-            }}
-          >
-            {props => (
-              <form onSubmit={props.handleSubmit}>
-                <div>
-                <TextField
-                  type="text"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.name}
-                  name="Email"
-                />
-                {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-                </div>
-                <div>
-                  <TextField
-                    type="text"
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.name}
-                    name="Password"
-                  />
-                  {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-                </div>
-                <div>
-                <Button type="submit">Submit</Button>
-                </div>
-              </form>
-            )}
-          </Formik>
-          </div>
-      </div>
-    )
+    }
+    {
+        return <div>kuksaft</div>;
+    }
 };
 
 export default Admin;
