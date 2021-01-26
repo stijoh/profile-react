cabf18aa7a2244c8ddb890f98b823584575f2f76
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-26 21:30:12 +0100

    some refactoring and added logout

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LoginForm from "../../components/LoginForm";
+import { Button } from "@material-ui/core";
 
 const Admin = () => {
-    if (!localStorage.getItem("token")) {
+    const localToken = localStorage.getItem("token");
+    const [token, setToken] = useState(localToken);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        if (token) setIsLoggedIn(true);
+    }, [token]);
+
+    if (!token)
         return (
             <div>
-                <LoginForm />
+                <LoginForm setToken={setToken} token={token} />
             </div>
         );
-    }
-    {
-        return <div>kuksaft</div>;
-    }
+
+    return (
+        isLoggedIn && (
+            <div>
+                <Button onClick={() => setToken(null)}>Logg ut</Button>
+            </div>
+        )
+    );
 };
 
 export default Admin;
