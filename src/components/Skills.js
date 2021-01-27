760d9bf9d102e2002a70cee383a8326624e49789
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-27 15:16:04 +0100

    refactored with react query and axios

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,27 @@
-const Skills = ({ skills }) => {
+import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const Skills = () => {
+    const { isLoading, error, data } = useQuery("fetchSkills", () =>
+        axios("http://192.168.0.100:3500/skills")
+    );
     return (
         <div>
             <div className="box-div2">
                 <h2>Fortunately I have some skills:</h2>
                 <div className="box-skills">
-                    <ul className="list-column">
-                        {skills.map((skill) => (
-                            <li key={skill.id}>{skill.skill}</li>
-                        ))}
-                    </ul>
+                    {error && <div>Something went wrong ...</div>}
+
+                    {isLoading ? (
+                        <div>Retrieving stians skills ...</div>
+                    ) : (
+                        <ul className="list-column">
+                            {data.data.map((skill) => (
+                                <li key={skill.id}>{skill.skill}</li>
+                            ))}
+                        </ul>
+                    )}
                 </div>
             </div>
         </div>
