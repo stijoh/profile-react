16df88dbbc5a0b03d6054f453251a9518ee4caae
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-27 15:38:44 +0100

    added loader spinner

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,9 @@
 import React from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { useQuery } from "react-query";
+import { CenterFocusStrong } from "@material-ui/icons";
 
 const Skills = () => {
     const { isLoading, error, data } = useQuery("fetchSkills", () =>
@@ -10,13 +13,21 @@ const Skills = () => {
         <div>
             <div className="box-div2">
                 <h2>Fortunately I have some skills:</h2>
-                <div className="box-skills">
+                <div className="">
                     {error && <div>Something went wrong ...</div>}
 
                     {isLoading ? (
-                        <div>Retrieving stians skills ...</div>
+                        <div>
+                            <Loader
+                                type="Puff"
+                                color="#00BFFF"
+                                height={100}
+                                width={100}
+                                timeout={3000} //3 secs
+                            />
+                        </div>
                     ) : (
-                        <ul className="list-column">
+                        <ul className="list-column box-skills">
                             {data.data.map((skill) => (
                                 <li key={skill.id}>{skill.skill}</li>
                             ))}
